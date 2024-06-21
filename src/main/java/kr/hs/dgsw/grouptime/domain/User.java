23f223d88ae5c6bcaea7ba9952ff76ba865bda8f
gwon11225 <gwon11225@gmail.com>
2024-06-21 12:11:44 +0900

    Feat :: user entity 추가

---
+++ src/main/java/kr/hs/dgsw/grouptime/domain/User.java
@@ -0,0 +1,24 @@
+package kr.hs.dgsw.grouptime.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
