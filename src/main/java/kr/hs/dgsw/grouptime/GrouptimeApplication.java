7110e6ec15617f7c4e91a8d19e9d546f97096b56
gwon11225 <gwon11225@gmail.com>
2024-06-21 11:11:26 +0900

    Init :: 최소 프로젝트

---
+++ src/main/java/kr/hs/dgsw/grouptime/GrouptimeApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.grouptime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouptimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrouptimeApplication.class, args);
+    }
+
+}
