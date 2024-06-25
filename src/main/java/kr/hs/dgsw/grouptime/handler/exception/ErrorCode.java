5782ce4a014fe9c507e7b831744e4ba4158a543a
gwon11225 <gwon11225@gmail.com>
2024-06-25 11:49:57 +0900

    Feat :: 예외 처리 추가

---
+++ src/main/java/kr/hs/dgsw/grouptime/handler/exception/ErrorCode.java
@@ -0,0 +1,16 @@
+package kr.hs.dgsw.grouptime.handler.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR(500, "서버 에러"),
+    USER_NOT_FOUNT(404, "유저를 찾을 수 없음"),
+    PASSWORD_IS_NOT_EQUAL(400, "비밀번호가 다름")
+    ;
+
+    private final Integer code;
+    private final String message;
+}
